852076873221f48dc3eb3631155c2bff22dc4f18
VDABMAARTEN <maarten.vanpraet@vdabcampus.be>
2025-04-22 09:44:22 +0200

    settings voor xml bean

--- src/main/java/be/vdab/xml/KwadraatService.java
+++ src/main/java/be/vdab/xml/KwadraatService.java
@@ -1,4 +1,10 @@
 package be.vdab.xml;
 
+import org.springframework.context.annotation.ImportResource;
+
+@ImportResource("beans.xml")
 public class KwadraatService {
+    int kwadraat(int getal){
+        return getal * getal;
+    }
 }
